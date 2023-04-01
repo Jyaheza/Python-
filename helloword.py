650487c275b9a0b185231249d5ebb285326ebaa5
Jyaheza Irakoze Maurice <64084059+Jyaheza@users.noreply.github.com>
2023-04-01 14:45:04 -0500

    Add files via upload

---
+++ helloword.py
@@ -0,0 +1,16 @@
+#python variables are cool
+from tokenize import Double
+
+
+x="Hello Word"
+y=str(4)
+z=float(4)
+m=int(4)
+print(y)
+print(z)
+print(m)
+if 5>4:
+    print(x)
+    print("error")
+else:
+    print("5<4")
